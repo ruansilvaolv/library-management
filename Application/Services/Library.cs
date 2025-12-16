546d6b24d9b46804f547e7da3f4bd66e88d3fbf4
Ruan <ruansilvaolv@gmail.com>
2025-12-16 20:37:20 -0300

    - Refactor: Refactors the structure of Library class.

--- Application/Services/Library.cs
+++ Application/Services/Library.cs
@@ -1,6 +1,7 @@
 using LibraryManagement.Domain.Entities;
 using LibraryManagement.Domain.Factories;
-using LibraryManagement.UI;
+using LibraryManagement.Application.Interfaces;
+using LibraryManagement.Services;
 
 namespace LibraryManagement.Application.Services
 {
@@ -16,40 +17,41 @@ namespace LibraryManagement.Application.Services
         }
 
         #region Properties
-          public List<Book> Books { get; }
-          public List<User> Users { get; }
-          public List<Loan> Loans { get; }
+        public List<Book> Books { get; }
+        public List<User> Users { get; }
+        public List<Loan> Loans { get; }
 
-          private IUIHelper _ui;
+        private IUIHelper _ui;
         #endregion
 
 
         #region Registration Books Methods
-          public void RegisterBook((string bookName, string author, string isbn, int publicationYear) bookData)
-          {
-              Validator.ValidateBook(bookData);
-              var book = BookFactory.CreateBook(bookData.bookName, bookData.author, bookData.isbn, bookData.publicationYear);
+        public void RegisterBook((string bookName, string author, string isbn, int publicationYear) bookData)
+        {
 
-              Books.Add(book);
-          }
-        #endregion)
+            Validator.ValidateBook(bookData);
+            var book = BookFactory.CreateBook(bookData.bookName, bookData.author, bookData.isbn, bookData.publicationYear);
+
+            Books.Add(book);
+        }
+        #endregion
 
 
         #region Registration Users Methods
-          public void RegisterUser((string userName, string userEmail, string userPhone, int userOpt) userData)
-          {
-              Validator.ValidateUser(userData);
-              var user = UserFactory.CreateUser(userData.userOpt, userData.userName, userData.userEmail, userData.userPhone);
+        public void RegisterUser((string userName, string userEmail, string userPhone, int userOpt) userData)
+        {
+            Validator.ValidateUser(userData);
+            var user = UserFactory.CreateUser(userData.userOpt, userData.userName, userData.userEmail, userData.userPhone);
 
-              Users.Add(user);
-          }
+            Users.Add(user);
+        }
         #endregion
 
 
         public void MakeLoan()
-        {}
+        { }
 
         public void ProcessReturn()
-        {}
+        { }
     }
 }
